b98f57a387ea28a4694d8d950d33745a173a1ca6
Pradeep Subramaniam <kspradeepbe@gmail.com>
2016-08-29 23:33:02 +0530

    Add ember cli mirage

---
+++ mirage/fixtures/videos.js
@@ -0,0 +1,43 @@
+export default {
+  'data': [
+    {
+      'id': '1',
+      'type': 'video',
+      'attributes': {
+        "title": "Building an Ember.js App - Part 1",
+        "description": "In this video we will start the task manager app. We will setup and configure Ember.js and create routes, templates and controllers ...",
+        "created-at": 1472324659962,
+        "thumbnail-image": 'assets/images/video_thumbnail_image_1.jpg',
+        "videoSource": '',
+        "category": 'Technology',
+        "duration": "0:39"
+      }
+    },
+    {
+      'id': '2',
+      'type': 'video',
+      'attributes': {
+        "title": 'Single Page Apps with Ember.js',
+        "description": "This talk will focus on Ember.js, a front-end javascript framework for creating ambitious web applications. HTML5 is frequently ...",
+        "created-at": 1472324886165,
+        "thumbnail-image": 'assets/images/video_thumbnail_image_2.jpg',
+        "videoSource": '',
+        "category": 'Technology',
+        "duration": "41:10"
+      }
+    },
+    {
+      'id': '3',
+      'type': 'video',
+      'attributes': {
+        "title": 'Ember.js Fundamentals',
+        "description": "Data down and actions up with Ember! Code examples from this video: ...",
+        "created-at": 1472324966658,
+        "thumbnail-image": 'assets/images/video_thumbnail_image_1.jpg',
+        "videoSource": '',
+        "category": 'Technology',
+        "duration": "49:54"
+      }
+    }
+  ]
+};
